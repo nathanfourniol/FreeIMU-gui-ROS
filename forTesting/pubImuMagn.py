22e6e07cb4f329cba07042fd6035a34de2921903
nathanfourniol <nathan.fourniol@ensta-bretagne.org>
2019-07-26 09:20:53 +0300

    file .json instead of .h

--- forTesting/pubImuMagn.py
+++ forTesting/pubImuMagn.py
@@ -1,12 +1,12 @@
 import rospy
 from sensor_msgs.msg import Imu
 from sensor_msgs.msg import MagneticField
-
+import random as rn
 
 rospy.init_node('PubIMU', anonymous=True)
 imu_topic = rospy.Publisher('/imu/imu', Imu, queue_size=10)
 magn_topic = rospy.Publisher('/imu/magn', MagneticField, queue_size=10)
-rate = rospy.Rate(4)
+rate = rospy.Rate(100)
 
 def _create_msg():
     imu_msg = Imu()
@@ -14,12 +14,12 @@ def _create_msg():
     
     imu_msg.header.stamp = rospy.Time.now() 
     magn_msg.header.stamp = rospy.Time.now() 
-    imu_msg.linear_acceleration.x = 2102. 
-    imu_msg.linear_acceleration.y = 2. 
-    imu_msg.linear_acceleration.z = 56.
-    magn_msg.magnetic_field.x = 78.
-    magn_msg.magnetic_field.y = 88.
-    magn_msg.magnetic_field.z = 98.
+    imu_msg.linear_acceleration.x = rn.uniform(-10,10) 
+    imu_msg.linear_acceleration.y = rn.uniform(-10,10) 
+    imu_msg.linear_acceleration.z = rn.uniform(-10,10)
+    magn_msg.magnetic_field.x = rn.uniform(-10,10)
+    magn_msg.magnetic_field.y = rn.uniform(-10,10)
+    magn_msg.magnetic_field.z = rn.uniform(-10,10)
     return imu_msg, magn_msg
 
 
